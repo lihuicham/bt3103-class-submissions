27ebeec3cb78c816d7d037075f86e21c3d035ea2
Li Hui Cham <lihuicham@gmail.com>
2023-02-27 18:46:05 +0800

    routing done

---
+++ lecture6/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <WelcomeCPP />
+  </div>
+</template>
+
+<script>
+import WelcomeCPP from '../components/WelcomeCPP.vue';
+  export default {
+    name: 'Home',
+    components: {
+      WelcomeCPP,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
